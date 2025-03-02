6eef79ab2713079127f71f4bcf32a69716a08d3c
iamhrk <iamhrk8@gmail.com>
2025-03-02 10:26:36 +0530

    Add clear button functionality

--- javascript-project/script.js
+++ javascript-project/script.js
@@ -6,17 +6,36 @@ let previousOperator = "";
 let operatorValue;
 let calculatedPreviuosResult = 0;
 let currentResult = 0;
-let canSetOperator = true;
+let canSetOperator = false;
+let isResultCalculated = false;
 answer.innerText = 0;
 
 const operand = document.querySelector(".numbers");
 const operator = document.querySelector(".operators");
 const equal = document.querySelector(".equal");
+const reset = document.querySelector(".clear-button");
+
+const resetCalculator = () => {
+  currentOperandValue = 0;
+  previousOperandValue = 0;
+  currentOperator = "";
+  previousOperator = "";
+  operatorValue;
+  calculatedPreviuosResult = 0;
+  currentResult = 0;
+  canSetOperator = false;
+  isResultCalculated = false;
+  answer.innerText = 0;
+};
 
 const displayResult = (event) => {
   event.stopPropagation();
   calculateCurrentResult();
-  answer.innerText = currentResult;
+  if (isResultCalculated) {
+    answer.innerText = currentResult;
+  } else {
+    answer.innerText = currentOperandValue;
+  }
 };
 
 const calculateCurrentResult = () => {
@@ -24,24 +43,21 @@ const calculateCurrentResult = () => {
     switch (previousOperator) {
       case "+":
         currentResult = previousOperandValue + currentOperandValue;
-        // alert(currentResult);
         break;
       case "-":
         currentResult = previousOperandValue - currentOperandValue;
-        // alert(currentResult);
         break;
       case "*":
         currentResult = previousOperandValue * currentOperandValue;
-        // alert(currentResult);
         break;
       case "/":
         currentResult = previousOperandValue / currentOperandValue;
-        // alert(currentResult);
         break;
       default:
         console.error("Not a valid operation.");
         break;
     }
+    isResultCalculated = true;
   }
 };
 
@@ -67,7 +83,7 @@ const setOperator = (event) => {
     calculateCurrentResult();
     currentOperator = event.target.value;
     previousOperandValue = currentOperandValue;
-    if (currentResult !== 0) {
+    if (isResultCalculated) {
       previousOperandValue = currentResult;
     }
     currentOperandValue = 0;
@@ -82,3 +98,4 @@ const setOperator = (event) => {
 operand.addEventListener("click", setcurrentOperandValue);
 operator.addEventListener("click", setOperator);
 equal.addEventListener("click", displayResult);
+reset.addEventListener("click", resetCalculator);
